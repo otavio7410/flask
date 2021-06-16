05e29e7b712547f3b8593f296d416924a9deb30e
Otávio Moraes <otaviomoraes357@gmail.com>
2021-06-16 20:56:06 -0300

    first commit

---
+++ flaskr.py
@@ -0,0 +1,90 @@
+# Tecnologia WEB
+# AC04 - Tutorial Flask
+# aluno: otavio.sousa@aluno.faculdadeimpacta.com.br
+
+
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+    abort, render_template, flash
+from contextlib import closing
+
+#configuração
+
+DATABASE = './tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+
+
+#criando a aplicação
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.from_envvar('CONFIG_FLASKR', silent=True)
+
+
+def conectar_bd():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def criar_bd():
+    with closing(conectar_bd()) as bd:
+        with app.open_resource('esquema.sql') as sql:
+            bd.cursor().executescript(sql.read().decode('utf-8'))
+        bd.commit()
+
+criar_bd()
+
+@app.before_request
+def pre_requisicao():
+    g.bd = conectar_bd()
+
+@app.teardown_request
+def encerrar_requisicao(exception):
+    g.bd.close()
+
+
+@app.route('/')
+def exibir_entradas():
+    sql = '''select titulo, texto from entradas order by id desc'''
+    cur = g.bd.execute(sql)
+    entradas = [dict(titulo=titulo, texto=texto)
+                    for titulo, texto in cur.fetchall()]
+    return render_template('exibir_entradas.html', entradas=entradas)
+
+@app.route('/inserir', methods=['POST'])
+def inserir_entrada():
+    if not session.get('logado'):
+        abort(401)
+    sql = '''insert into entradas(titulo, texto) values (?, ?)'''
+    g.bd.execute(sql, [request.form['titulo'], request.form['texto']])
+    g.bd.commit()
+    flash('Nova entrada registrada com sucesso')
+    return redirect(url_for('exibir_entradas'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    erro = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            erro = 'Usuário inválido'
+        elif request.form['password'] != app.config['PASSWORD']:
+            erro = 'Senha inválida'
+        else:
+            session['logado'] = True
+            flash('Login OK')
+            return redirect(url_for('exibir_entradas'))
+    return render_template('login.html', erro=erro)
+
+@app.route('/logout')
+def logout():
+    session.pop('logado', None)
+    flash('Logout OK')
+    return redirect(url_for('exibir_entradas'))
+
+
+if __name__ == '__main__':
+    app.run()
+    
